8977ace0973180b8f62942b9db5b76e274318b38
Filipi Arruda <filipi.arruda@digi.ag>
2025-07-23 15:27:05 -0300

    feat: create models and migrations to redirect and redirect logs tables and models

---
+++ app/Models/Redirect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Redirect extends Model
+{
+    protected $fillable = ['code', 'redirect_url', 'active'];
+
+    /**
+     * Get the redirect URL.
+     *
+     * @return string
+     */
+    public function getRedirectUrlAttribute()
+    {
+        return $this->attributes['redirect_url'];
+    }
+
+    /**
+     * Check if the redirect is active.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+}
